7a8092db2d4a74289fcc00ba1b78ce269bda841a
GayathriVignesh-512 <129622212+GayathriVignesh-512@users.noreply.github.com>
2023-04-01 15:09:33 -0400

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebPackPlugin({
+ template: './src/index.html',
+ filename: './index.html'
+});
+
+module.exports = {
+
+mode: 'development',
+devServer:{
+port:"7070"
+},
+  module: {
+    rules: [{
+        test: /\.(js|jsx|ts|tsx)$/,
+   exclude: /node_modules/,
+   use: {
+     loader: 'babel-loader'
+   }
+ },
+  {
+   test: /\.(css|scss)$/,
+   use: ['style-loader', 'css-loader', "sass-loader"]
+  },
+  {
+   test: /\.(png|j?g|svg|gif)?$/,
+   use: ["file-loader"]
+  }
+]},
+ plugins: [htmlPlugin]
+};
